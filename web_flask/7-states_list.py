02916274b052a16361625e47447c1b5d3bf089eb
Nurudeen <nurudeensultan28@gmail.com>
2023-04-24 08:49:05 +0100

    Documented 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,12 +14,14 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
+    """display html page from 7-states_list.html"""
     states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exception):
+    """Close storage"""
     storage.close()
 
 
